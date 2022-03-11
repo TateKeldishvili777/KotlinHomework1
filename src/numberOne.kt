64fccbc4cb1c4a29ab4b04a2f4d8ab0342fcdf34
Tate777331 <estate.keldishvili.1@btu.edu.ge>
2022-03-12 00:39:16 +0400

    fixed number one

--- src/numberOne.kt
+++ src/numberOne.kt
@@ -1,17 +1,17 @@
 fun main() {
-    val cars = arrayOf(7, 2, 3, 4)
-    var sum = 0
+    val arr = arrayOf(7, 2, 3, 4, 5, 6, 7, 8, 10, 32, 45)
+    var sum = 0F
     var i = 0
-    var cuant = 0
-    var size = cars.size
+    var cuant = 0F
+    var size = arr.size
     while (i < size) {
         if (i%2==0){
-            sum += cars[i]
+            sum += arr[i]
             cuant+=1
         }
         i++
     }
-    var answer = sum/cuant
+    val answer = sum/cuant
 
     println(answer)
 
